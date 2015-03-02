5a964d335f645eb9efc55f9ccf04b7077e2a154b
Josh Beveridge <origin@joshdr.ink>
2015-03-01 21:27:58 -0400

    Navigation Enabled, Formatting

--- content/themes/geode/assets/js/nav.js
+++ content/themes/geode/assets/js/nav.js
@@ -8,53 +8,38 @@
 
 // Navigation ==================================================================
 
-    // // Menu Open
-    // $('.navigation-button').click(function() {
-    //
-    //     if($('.navigation').hasClass('active')) {
-    //         return false;
-    //     }
-    //     else {
-    //         $('.navigation-button').css('display', 'none');
-    //         $('.label-navigation').removeClass('active');
-    //         $('.navigation').addClass('active');
-    //         $('.navigation-list-item').addClass('active');
-    //         $('.navigation-close').addClass('active');
-    //         $('.navigation-close-exterior').css('display', 'block');
-    //     }
-    //
-    // });
-    //
-    // // Menu Clase
-    // $('.navigation-close').click(function() {
-    //
-    //     if($('.navigation').hasClass('active')) {
-    //         $('.navigation-button').css('display', 'block');
-    //         $('.navigation-close').removeClass('active');
-    //         $('.navigation-close-exterior').css('display', 'none');
-    //         $('.navigation').removeClass('active');
-    //         $('.navigation-list-item').removeClass('active');
-    //         $('.label-navigation').addClass('active');
-    //     }
-    //     else {
-    //         return false;
-    //     }
-    //
-    // });
-    //
-    // // Menu Clase (Exterior)
-    // $('.navigation-close-exterior').click(function() {
-    //
-    //     if($('.navigation').hasClass('active')) {
-    //         $('.navigation-button').css('display', 'block');
-    //         $('.navigation-close').removeClass('active');
-    //         $('.navigation-close-exterior').css('display', 'none');
-    //         $('.navigation').removeClass('active');
-    //         $('.navigation-list-item').removeClass('active');
-    //         $('.label-navigation').addClass('active');
-    //     }
-    //     else {
-    //         return false;
-    //     }
-    //
-    // });
+    // Menu Open
+    $('.navigation-button').click(function() {
+
+        if($('.wrapper-navigation').hasClass('active')) {
+            return false;
+        }
+        else {
+            $('.navigation-button').css('display', 'none');
+            $('.label-navigation').removeClass('active');
+            $('.wrapper-navigation').addClass('active');
+            $('.navigation-list-item').addClass('active');
+            $('.navigation-close').addClass('active');
+            $('.navigation-overlay').addClass('active');
+            $('.navigation-close-exterior').css('display', 'block');
+        }
+
+    });
+
+    // Menu Clase
+    $('.navigation-close').click(function() {
+
+        if($('.wrapper-navigation').hasClass('active')) {
+            $('.navigation-button').css('display', 'block');
+            $('.navigation-close').removeClass('active');
+            $('.navigation-overlay').removeClass('active');
+            $('.navigation-close-exterior').css('display', 'none');
+            $('.wrapper-navigation').removeClass('active');
+            $('.navigation-list-item').removeClass('active');
+            $('.label-navigation').addClass('active');
+        }
+        else {
+            return false;
+        }
+
+    });
